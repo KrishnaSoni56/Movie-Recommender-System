13d4a70a5b0d32f56aef7c59dfe661d1ceef265d
KrishnaSoni56 <soni2005krishna26@gmail.com>
2024-02-27 19:13:35 +0530

    Creating Website for recommendation system

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import streamlit as st
+import pickle
+movies_list = pickle.load(open('movies_list.pkl' , 'rb'))  # DataFrame
+movies_list = movies_list['title'].values
 
 st.title('Movie Recommender System')
+
+# Creating form
+# Creating SelectBox for movies_list
+
+option = st.selectbox(
+    'How would you' , movies_list
+)
